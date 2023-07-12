e9f789886c20692236ad3f567e621ef3b837919f
Eren <118849784+KeIrNeGn@users.noreply.github.com>
2023-07-12 21:57:50 +0300

    Add files via upload

---
+++ testsite/index.js
@@ -0,0 +1,102 @@
+function reveal1() {
+    var reveals = document.querySelectorAll(".info_text_1");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  function reveal2() {
+    var reveals = document.querySelectorAll(".info_text_2");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 400;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  function reveal3() {
+    var reveals = document.querySelectorAll(".content");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 400;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal1);
+  window.addEventListener("scroll", reveal2);
+  window.addEventListener("scroll", reveal3);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Kundura Pazarlama', 'Afgan Ticareti', 'Horoz Kavgasi'],
+  'text',
+  ['tomato', 'gold', 'orange']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 400) //first wait --                                                                  ------------------
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 200)      //last wait    --                                                           ------------------
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 80) //typing speed  --                                                                   ------------------
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400) //underscore --                                                                      ------------------
+}
+
